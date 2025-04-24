83716199a5e5992dc5225534ff5788a64fcfcfac
Maciej Bazyl <1i2022maciejb2@gmail.com>
2025-04-24 12:01:43 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+const display = document.querySelector("main");
+const button = document.querySelector("button"); // przy większej ilości przycisków najlepiej użyć klas
+
+let i = 1;
+
+button.addEventListener("click", function (event) {
+    const paragraf = document.createElement("p");
+    paragraf.innerText = `nowy paragraf nr ${i}`;
+
+    paragraf.addEventListener("click", function () {
+        this.remove();
+    });
+
+    display.append(paragraf);
+    i++;
+});
+    
