db5558fcdeafa253db3ce567d1dc34d45d39fe07
Maciej Bazyl <1i2022maciejb2@gmail.com>
2025-04-24 12:19:38 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const display = document.querySelector("main");
 const button = document.querySelector("button"); // przy większej ilości przycisków najlepiej użyć klas
 
+
 let i = 1;
 
 button.addEventListener("click", function (event) {
